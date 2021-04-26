db0f2757359f37a2482b418ef0293d624d67e32e
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-25 17:33:22 -0700

    added sorting numbers

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const app = document.getElementById('app')
 // Your main array
 const mixed = ["Banana", "Orange", "Apple", 40, 100, 1, 5, 25, 10, 400, 34, 299, 150, "Mango", "Saab", "Volvo", "BMW"];
 
-// Sort strings 
+// SORT STRINGS
 const stringSorting = (array) => {
     return array.sort()
 }
 
-// Call the function
-const result = stringSorting(mixed)
+// // Call the function
+// const result = stringSorting(mixed)
 
-// Show the function result
+// // Show the function result
+// app.innerHTML = JSON.stringify(result)
+
+// NUMBER SORTING
+const numberSorting = (array) => {
+    return array.sort((a, b) => a - b)
+}
+const result = numberSorting(mixed)
 app.innerHTML = JSON.stringify(result)
-
